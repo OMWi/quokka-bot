410c9421d122230f48653f5063d47d76f3110b32
OMWi <bsuiromwi@gmail.com>
2021-06-02 22:27:12 +0300

    checking git all

--- app.py
+++ app.py
@@ -21,31 +21,24 @@ def respond():
 
    # Telegram understands UTF-8, so encode text for unicode compatibility
    text = update.message.text.encode('utf-8').decode()
-   # for debugging purposes only
-   print("got text message :", text)
-   # the first time you chat with the bot AKA the welcoming message
+   
    if text == "/start":
-       # print the welcoming message
        bot_welcome = "hi, there"
-       # send the welcoming message
-       bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+       bot.sendMessage(chat_id=chat_id, text=bot_welcome)
 
    return 'ok'
 
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
-    # we use the bot object to link the bot to our app which live
-    # in the link provided by URL
-    s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
-    # something to let us know things work
-    if s:
-        return "webhook setup ok"
+    res = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    if res:
+        return "webhook setup successful"
     else:
         return "webhook setup failed"
 
 @app.route('/')
 def index():
-    return "Fancy homepage"
+    return "Quokka bot homepage"
 
 
 if __name__ == '__main__':
